ade0865c65076cb5f270e4c4fb31f703071861cd
casey <494294315@qq.com>
2021-12-22 19:46:26 +0800

    fix bug

--- static/export2web/src/index.js
+++ static/export2web/src/index.js
@@ -1,83 +1,80 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import { ConfigProvider, DatePicker, message } from 'antd';
-
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
-
-// 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/lib/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
+import { Layout, Menu ,Form , Radio , Input} from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Upload, Button } from 'antd';
 
-moment.locale('zh-cn');
+const { Header, Content, Footer, Sider } = Layout;
 
-const App = () => {
-    return (
-        <ConfigProvider>
-            <Layout>
-                <Header className="header">
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
-                <Layout>
-                    <Sider width={200} className="site-layout-background">
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%', borderRight: 0 }}
-                        >
-                            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                                <Menu.Item key="3">option3</Menu.Item>
-                                <Menu.Item key="4">option4</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                                <Menu.Item key="5">option5</Menu.Item>
-                                <Menu.Item key="6">option6</Menu.Item>
-                                <Menu.Item key="7">option7</Menu.Item>
-                                <Menu.Item key="8">option8</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                                <Menu.Item key="9">option9</Menu.Item>
-                                <Menu.Item key="10">option10</Menu.Item>
-                                <Menu.Item key="11">option11</Menu.Item>
-                                <Menu.Item key="12">option12</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Layout style={{ padding: '0 24px 24px' }}>
-                        <Breadcrumb style={{ margin: '16px 0' }}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <Content
-                            className="site-layout-background"
-                            style={{
-                                padding: 24,
-                                margin: 0,
-                                minHeight: 280,
-                            }}
-                        >
-                            Content
-                        </Content>
-                    </Layout>
-                </Layout>
-            </Layout>
-        </ConfigProvider>
-    );
-};
+ReactDOM.render(
+    <Layout>
+        <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={broken => {
+                console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+                console.log(collapsed, type);
+            }}
+        >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onChange={() => {
 
-render(<App />, document.getElementById('root'));
+            }}>
+                <Menu.Item key="1" icon={<UserOutlined />}>
+                    {/* 价格表导出 */}
+                    AAAAAA
+                </Menu.Item>
+                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                    {/* 导出采购表 */}
+                    BBBBB
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout>
+            <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+            <Content style={{ margin: '24px 16px 0' }}>
+                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                    <Form
+                        labelCol={{span : 4}}
+                        wrapperCol={{span : 4}}
+                        // layout={formLayout}
+                        layout='horizontal'
+                        // form={form}
+                        // initialValues={{ layout: formLayout }}
+                        // onValuesChange={onFormLayoutChange}
+                    >
+                        <Form.Item label="Field A">
+                            <Upload action={'https://www.mocky.io/v2/5cc8019d300000980a055e76'}>
+                                <Button icon={<UploadOutlined />}>Upload</Button>
+                            </Upload>
+                        </Form.Item>
+
+                        <Form.Item label="Form Layout" name="layout">
+                            <Radio.Group>
+                                <Radio.Button value="horizontal">Horizontal</Radio.Button>
+                                <Radio.Button value="vertical">Vertical</Radio.Button>
+                                <Radio.Button value="inline">Inline</Radio.Button>
+                            </Radio.Group>
+                        </Form.Item>
+                        <Form.Item label="Field A">
+                            <Input placeholder="input placeholder" />
+                        </Form.Item>
+                        <Form.Item label="Field B">
+                            <Input placeholder="input placeholder" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary">Submit</Button>
+                        </Form.Item>
+                    </Form>
+
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Created By NB Team</Footer>
+        </Layout>
+    </Layout>,
+    document.getElementById('container'),
+);
