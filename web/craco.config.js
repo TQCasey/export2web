6e19b898dc7a4eb59c4ef2646f652a583dc1f16b
casey <494294315@qq.com>
2021-12-27 15:26:43 +0800

    OK done !

--- web/craco.config.js
+++ web/craco.config.js
@@ -11,26 +11,26 @@ const outputDir = path.resolve(__dirname, '' + outname);
 const IndexHtml = path.resolve(__dirname, 'public', 'index.html');
 const DestIndexHtml = path.resolve(outputDir, 'index.html');
 
-const cssLoaderConfiguration = {
-    test: /\.css|\.less$/,
-    use: ['style-loader',
-        'css-loader'],
-}
+// const cssLoaderConfiguration = {
+//     test: /\.css|\.less$/,
+//     use: ['style-loader',
+//         'css-loader'],
+// }
 
-const scssLoaderConfiguration = {
-    test: /\.scss$/,
-    use: [
-        {
-            loader: "style-loader" 
-        }, 
-        {
-            loader: "css-loader" 
-        }, 
-        {
-            loader: "sass-loader" 
-        }
-    ]
-}
+// const scssLoaderConfiguration = {
+//     test: /\.scss$/,
+//     use: [
+//         {
+//             loader: "style-loader" 
+//         }, 
+//         {
+//             loader: "css-loader" 
+//         }, 
+//         {
+//             loader: "sass-loader" 
+//         }
+//     ]
+// }
 
 module.exports = {
 
@@ -44,8 +44,13 @@ module.exports = {
             // paths.appPath='public'
             paths.appBuild = outname
             webpackConfig.output = {
-                filename: whenDev(() => 'static/bundle.[fullhash:8].js', 'static/[name].[fullhash:8].js'),
-                chunkFilename: '[name].[fullhash:8].js',
+                // filename: whenDev(() => 'js/bundle.[fullhash:8].js', 'js/[name].[fullhash:8].js'),
+                // chunkFilename: 'js/[name].[fullhash:8].js',
+
+
+                filename: whenDev(() => 'js/bundle.js', 'js/[name].js'),
+                chunkFilename: 'js/[name].js',
+
                 path: outputDir, // 修改输出文件目录
                 publicPath: '/',
                 clean: true,
@@ -53,23 +58,33 @@ module.exports = {
 
             webpackConfig.plugins = [
                 new MiniCssExtractPlugin({
-                    filename: "static/[name].[fullhash:8].css", // change this RELATIVE to your output.path!
+                    // filename: "css/[name].[fullhash:8].css", // change this RELATIVE to your output.path!
+                    filename: "css/[name].css", // change this RELATIVE to your output.path!
                 })
                 ,
-                new HtmlWebpackPlugin({
-                    filename: DestIndexHtml,
-                    chunks: [
-                        'main',
-                    ],
-                    template: IndexHtml,
-                    inject: "body",
-                    // meta : 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=0, charset=utf-8',
-                    minify: {
-                        caseSensitive: false,
-                        collapseBooleanAttributes: true,
-                        collapseWhitespace: true
-                    },
-                }),
+                // new HtmlWebpackPlugin({
+                //     filename: DestIndexHtml,
+                //     chunks: [
+                //         'main',
+                //     ],
+                //     template: IndexHtml,
+                //     inject: "body",
+                //     // meta : 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=0, charset=utf-8',
+                //     minify: {
+                //         caseSensitive: false,
+                //         collapseBooleanAttributes: true,
+                //         collapseWhitespace: true
+                //     },
+                // }),
+
+                new CopyWebpackPlugin ({
+                    patterns : [
+                        {
+                            from : path.resolve (__dirname,"public","index.html"),
+                            to : outputDir,
+                        }
+                    ]
+                })
 
             ];
 
